d6f13a6986c5915d79abdb9a33e0d6c31ce8fa13
Yusuke Kawamoto <novogrammer@gmail.com>
2022-08-08 03:24:27 +0900

    とりあえずの地形

--- wasm/src/terrain_updater.rs
+++ wasm/src/terrain_updater.rs
@@ -1,6 +1,6 @@
 use crate::block::Block;
 
-pub fn terrain_updater_a(global_position: &glam::Vec3, time: f64) -> Block {
+pub fn terrain_updater_first(global_position: &glam::Vec3, time: f64) -> Block {
     let mut next_cell = Block::Air;
     if global_position.length() < (32.0 * (time.sin() * 0.5 + 0.5)) as f32 {
         next_cell = Block::Sand;
@@ -9,6 +9,22 @@ pub fn terrain_updater_a(global_position: &glam::Vec3, time: f64) -> Block {
         < (32.0 * (time.sin() * 0.5 + 0.5)) as f32
     {
         next_cell = Block::Metal;
+    }
+    next_cell
+}
+
+pub fn terrain_updater_a(global_position: &glam::Vec3, time: f64) -> Block {
+    let mut next_cell = Block::Air;
+    let ground_level = (global_position.x() * 5.0 + time as f32 * 30.0)
+        .to_radians()
+        .sin()
+        * (global_position.z() * 5.0).to_radians().sin()
+        * 9.0
+        - 10.0;
+    if global_position.y() < ground_level - 3.0 {
+        next_cell = Block::Rock;
+    } else if global_position.y() < ground_level {
+        next_cell = Block::Sand;
     }
     next_cell
 }
